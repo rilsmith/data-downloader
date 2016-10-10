4b72bd6634d82615f1ab356389442c0192be6be1
rilsmith <jrsmith.byu@gmail.com>
2016-10-09 18:39:52 -0600

    Refactor names, remove fililngs after parsing

--- EDGAR/FilingParser.py
+++ EDGAR/FilingParser.py
@@ -1,3 +1,4 @@
+import os
 import json
 import pika
 import pandas
@@ -12,25 +13,26 @@ class ParseDownload:
         self.parsed_queue = self.channel.queue_declare(queue='parsed')
 
     @staticmethod
-    def consume(download_entry):
+    def process(download_entry):
         data = json.loads(download_entry)
         us_gaap = UsGaap(data['file'])
         data_frame = pandas.DataFrame(data=us_gaap.gaap)
         return data['file'], data_frame
 
-    def publish(self, parsed_file):
-        data = {'file': parsed_file}
-        self.channel.basic_publish(exchange='', routing_key='parsed', body=json.dumps(data))
-
     @staticmethod
     def store(parsed_file, data_frame):
         data_frame.to_pickle(re.sub('\..*', '.pkl', parsed_file))
 
-    def process(self, download_entry):
-        parsed_file, df = self.consume(download_entry)
+    def publish(self, parsed_file):
+        data = {'file': parsed_file}
+        self.channel.basic_publish(exchange='', routing_key='parsed', body=json.dumps(data))
+
+    def consume(self, download_entry):
+        parsed_file, df = self.process(download_entry)
         self.store(parsed_file, df)
+        os.remove(parsed_file)
         self.publish(parsed_file)
 
     def run(self):
-        self.channel.basic_consume(self.process, queue='downloaded', no_ack=True)
+        self.channel.basic_consume(self.consume, queue='downloaded', no_ack=True)
         self.channel.start_consuming()
